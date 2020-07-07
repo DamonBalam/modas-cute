ddfe76027960bf9e9ae3aa94830e927574a706a2
Arturo Saldivar <damonbalam@gmail.com>
2020-07-07 00:09:38 -0500

    Fix Cards

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,18 +4,50 @@
             <v-col cols="12" class="text-center d-block my-5">
                 <span class="headline  font-weight-bold">CATÁLOGO </span>
             </v-col>
-            <v-col cols="6" md="6" class="px-2">
+            <v-col cols="6" md="3" class="px-2">
                 <v-card>
-                    <v-card-text>Soju Sabor Original - $115.00 mxn</v-card-text>
-
+                    <v-card-text class="d-block text-center font-weight-bold black--text title-1"
+                        >Chum Churum Korean Soju - Original</v-card-text
+                    >
                     <v-img src="../assets/articulos/Soju.jpg" />
+                    <v-card-text class="d-block text-center font-weight-bold black--text"
+                        >$ 115.00 mxn</v-card-text
+                    >
                 </v-card>
             </v-col>
 
-            <v-col cols="6" md="6" class="px-2">
+            <v-col cols="6" md="3" class="px-2">
                 <v-card>
-                    <v-card-text>Soju Sabor Original - $115.00 mxn</v-card-text>
+                    <v-card-text class="d-block text-center font-weight-bold black--text title-1"
+                        >Chum Churum Korean Soju - Fresa</v-card-text
+                    >
                     <v-img src="../assets/articulos/Soju.jpg" />
+                    <v-card-text class="d-block text-center font-weight-bold black--text"
+                        >$ 125.00 mxn</v-card-text
+                    >
+                </v-card>
+            </v-col>
+
+            <v-col cols="6" md="3" class="px-2">
+                <v-card>
+                    <v-card-text class="d-block text-center font-weight-bold black--text title-1"
+                        >Chum Churum Korean Soju - Manzana</v-card-text
+                    >
+                    <v-img src="../assets/articulos/Soju.jpg" />
+                    <v-card-text class="d-block text-center font-weight-bold black--text"
+                        >$ 125.00 mxn</v-card-text
+                    >
+                </v-card>
+            </v-col>
+            <v-col cols="6" md="3" class="px-2">
+                <v-card>
+                    <v-card-text class="d-block text-center font-weight-bold black--text title-1"
+                        >Chum Churum Korean Soju - Durazno</v-card-text
+                    >
+                    <v-img src="../assets/articulos/Soju.jpg" />
+                    <v-card-text class="d-block text-center font-weight-bold black--text"
+                        >$ 125.00 mxn</v-card-text
+                    >
                 </v-card>
             </v-col>
         </v-row>
