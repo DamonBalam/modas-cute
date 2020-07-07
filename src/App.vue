e28ff9faa6811c9e660e59f7da7684afde0e2386
Arturo Saldivar <damonbalam@gmail.com>
2020-07-06 23:31:36 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-app>
+        <v-app-bar app :color="colorPrincipal" dark>
+            <div class="d-flex align-center">
+                <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+
+                <v-icon left>mdi-storefront</v-icon>
+                <span class="font-weight-bold headline">MODA'S CUTE</span>
+            </div>
+
+            <v-spacer></v-spacer>
+
+            <v-btn href="https://www.facebook.com/ModasCuteOficial/" target="_blank" text>
+                <span class="mr-2">Ir a Facebook</span>
+                <v-icon>mdi-facebook</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-content> </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data: () => ({
+            //
+        }),
+        computed: {
+            colorPrincipal() {
+                return this.$store.state.colorPrincipal;
+            },
+        },
+    };
+</script>
