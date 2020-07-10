071263207c064ef5635f2272fe22b524945a844a
Arturo Saldivar <damonbalam@gmail.com>
2020-07-10 12:19:52 -0500

    add ramune

--- src/views/Licor.vue
+++ src/views/Licor.vue
@@ -1,161 +1,187 @@
 <template>
-    <v-container>
-        <v-row dense justify="center" align-content="center">
-            <v-col cols="12" class="text-center d-block my-2">
-                <span class="headline  font-weight-bold">LICORES 🍾🍷</span>
-            </v-col>
-            
-            <!-- Soju -->
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Chum Churum Korean Soju - Original 360ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Original.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 115.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Chum Churum Korean Soju - Fresa 360ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Fresa.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 125.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Chum Churum Korean Soju - Manzana 360ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Manzana.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 125.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Chum Churum Korean Soju - Durazno 360ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Durazno.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 125.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <!-- Sake -->
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Ozeki Nigori Cloudy Sake 375ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Ozeki_Sake.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 160.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Sayuri Nigori Sake 300ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Sayuri_Sake.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 250.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Hakushika Junmai Ginjo Sake 300ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Hakushika.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 250.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Yamamoto Honke Sake 180ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Yamamoto.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 130.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Cerveza Sapporo Premium 355ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Sapporo.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 105.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Lotte Licor de Ginseng Coreano 750 ml</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/Korean_Ginseng_Wine.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 990.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-            <v-col cols="6" md="3" class="px-2">
-                <v-card>
-                    <v-card-text
-                        class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
-                        >Vaso para Soju</v-card-text
-                    >
-                    <v-img aspect-ratio="1" src="../assets/articulos/vasosoju.jpg" />
-                    <v-card-text
-                        class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
-                        >$ 50.00 mxn</v-card-text
-                    >
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row dense justify="center" align-content="center">
+      <v-col cols="12" class="text-center d-block my-2">
+        <span class="headline  font-weight-bold">LICORES 🍾🍷</span>
+      </v-col>
+
+      <!-- Soju -->
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Chum Churum Korean Soju - Original 360ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Original.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 115.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Chum Churum Korean Soju - Fresa 360ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Fresa.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 125.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Chum Churum Korean Soju - Manzana 360ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Manzana.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 125.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Chum Churum Korean Soju - Durazno 360ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Chum_Soju_Durazno.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 125.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Chum Churum Korean Soju - Uva 360ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Chum_uva.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 125.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Sangaria Ramune Melon 200 ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/ramune-melon.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 62.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <!-- Sake -->
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Ozeki Nigori Cloudy Sake 375ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Ozeki_Sake.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 160.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Sayuri Nigori Sake 300ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Sayuri_Sake.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 250.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Hakushika Junmai Ginjo Sake 300ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Hakushika.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 250.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Yamamoto Honke Sake 180ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Yamamoto.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 130.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Cerveza Sapporo Premium 355ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Sapporo.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 105.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Lotte Licor de Ginseng Coreano 750 ml</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/Korean_Ginseng_Wine.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 990.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3" class="px-2">
+        <v-card>
+          <v-card-text
+            class="d-block text-center font-weight-bold white--text title-1 deep-purple lighten-3"
+            >Vaso para Soju</v-card-text
+          >
+          <v-img aspect-ratio="1" src="../assets/articulos/vasosoju.jpg" />
+          <v-card-text
+            class="d-block text-center font-weight-bold black--text deep-purple lighten-4"
+            >$ 50.00 mxn</v-card-text
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    export default {};
+  export default {};
 </script>
 
 <style lang="scss" scoped></style>
